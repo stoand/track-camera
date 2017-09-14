bb56dbd4afd22f291095c23995aba7f50a4f3316
Andreas Stocker <shadowstep7@gmail.com>
2017-09-14 13:06:23 +0300

    use gcc only in build

--- webm_parser_glue/build_test.sh
+++ webm_parser_glue/build_test.sh
@@ -1,4 +1,4 @@
 # Requires libwebm to be installed globally
 # as well as catch - https://github.com/philsquared/Catch/blob/master/docs/tutorial.md
 
-clang++ -g -O0 -std=c++14 webm_parser_glue_test.cc webm_parser_glue.cc -lwebm -lvpx -o webm_parser_glue_test
+g++ -g -O0 -std=c++14 webm_parser_glue_test.cc webm_parser_glue.cc -lwebm -lvpx -o webm_parser_glue_test
